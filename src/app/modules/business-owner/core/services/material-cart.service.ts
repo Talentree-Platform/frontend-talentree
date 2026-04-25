d99989987d66d8dbfef1a7cc83b08314a57a112f
A'laa <112674763+Alaa-Hisham1@users.noreply.github.com>
2026-04-25 17:59:49 +0300

    Update apiUrl to point to the new backend endpoint

--- src/app/modules/business-owner/core/services/material-cart.service.ts
+++ src/app/modules/business-owner/core/services/material-cart.service.ts
@@ -10,7 +10,7 @@ import { BasketData, BasketItem } from '../interfaces/imaterial-cart';
 export class MaterialCartService {
 
   private readonly http =inject(HttpClient);
-  public apiUrl='/api';
+  public apiUrl='https://backtalentree.runasp.net/api';
     private countSubject = new BehaviorSubject<number>(0);
   count$ = this.countSubject.asObservable();
 
