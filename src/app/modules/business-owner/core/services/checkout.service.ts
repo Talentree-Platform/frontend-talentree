f3410ed05d73ee3be70a5c6f5c99c3ed0c1df11a
A'laa <112674763+Alaa-Hisham1@users.noreply.github.com>
2026-06-11 15:08:30 +0300

    Change API URL in CheckoutService
    Updated the API URL for the CheckoutService to point to the new backend endpoint.

--- src/app/modules/business-owner/core/services/checkout.service.ts
+++ src/app/modules/business-owner/core/services/checkout.service.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 export class CheckoutService {
 
   constructor(private http:HttpClient) { }
-  private readonly apiUrl = '/api';
+  private readonly apiUrl = 'https://backtalentree.runasp.net/api';
 
   checkout(deliveryAddress :string , deliveryCity:string , deliveryCountry:string , contactPhone:string):Observable<ApiResponse<MaterialOrder>>{
       return this.http.post<ApiResponse<MaterialOrder>>(`${this.apiUrl}/MaterialOrder/checkout` , {"deliveryAddress":deliveryAddress ,"deliveryCity":deliveryCity , "deliveryCountry" : deliveryCountry , "contactPhone":contactPhone})
